bde0e3d0a942b9fe7bb9df4236089104d761a2cb
MaPf06 <pfeutimarcel@gmail.com>
2018-07-03 10:25:53 +0100

    Wörks kinda better

--- WissPlayStore/src/menu/WissPlayStore.java
+++ WissPlayStore/src/menu/WissPlayStore.java
@@ -1,6 +1,10 @@
 package menu;
 
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import games.moorhuhn.controllers.Moorhuhn;
 import games.schiffliversaenken.schiffeversenken.Schiffliversenken;
 import games.tetris.controls.TetrisStart;
@@ -9,30 +13,50 @@ import processing.core.PApplet;
 /**
  * 
  * @author MaPf06
- *
+ * [Ljava.lang.String;@1540e19d
+ * 
  */
 
 public class WissPlayStore extends PApplet {
 	
 	String[] gameName = {"Tetris","Moorhuhn","Schiffliversänkä","TextAdventure N/A"};
 	Button[] button;
-	int fontsize;
-	int size = 0;
+	String applicationName;
 	
-	Thread tetris;
-	
-
 	public static void main(String[] args) {
-		PApplet.main("menu.WissPlayStore");
+		if (args.length == 0) {
+			PApplet.main("menu.WissPlayStore");
+		} else {
+			String arg = args[0];
+			switch (arg) {
+			case "Tetris":
+				TetrisStart.myMain();
+				break;
+			case "Moorhuhn":
+				Moorhuhn.myMain();
+				break;
+			case "Schiffliversänkä":
+				Schiffliversenken.myMain();
+				break;
+			case "SUVSE":
+				break;
+			}
+		}
 	}
-	
+	//Sets up the window
 	public void settings() {
 		size(800,800);
 	}
-	
+	//Sets up the buttons
 	public void setup() {
+		try {
+			setApplicationName();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		width = 800;
-		size = (width-(10*gameName.length)+10)/gameName.length;
+		int size = (width-(10*gameName.length)+10)/gameName.length;
 		button = new Button[gameName.length];
 		for (int i = 0; i < button.length; i++) {
 			button[i] = new Button(this, (size+10)*i,10,size,size);
@@ -42,6 +66,7 @@ public class WissPlayStore extends PApplet {
 		}
 	}
 	
+	//Draws the GUI
 	public void draw() {
 		background(75);
 		for (int i = 0; i < button.length; i++) {
@@ -51,37 +76,40 @@ public class WissPlayStore extends PApplet {
 		}
 	}
 
-	//W
+	//When the mouse gets clicked it will start the game
 	public void mouseClicked() {
-		startGame();
-	}
-	
-	public void startGame() {
-		if (button[0].checkOnButton(mouseX, mouseY)) {
-			//thread("startTetris");
-			tetris = new Thread(new TetrisStart());
-			tetris.start();
-		}
-		
-		if (button[1].checkOnButton(mouseX, mouseY)) {
-			thread("startMoorhuhn");
-		}
-		
-		if (button[2].checkOnButton(mouseX, mouseY)) {
-			thread("startSchiffliversenken");
+		try {
+			startGame();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
+	//Starts the game
+	//This application will be startet with the game name as args
+	public void startGame() throws IOException {
+		String gameName = null;
+		for (int i = 0; i < button.length; i++) {
+			if (button[i].checkOnButton(mouseX,mouseY)) {
+				gameName = button[i].text;
+				i = button.length;
+			}
+		}
+		if (gameName != null) {
+			Runtime rt = Runtime.getRuntime();
+			rt.exec("cmd.exe /c start java -jar "+applicationName+" "+gameName);
+		}
+	}
 	
-	public void startTetris() {
-		System.out.println("Test1");
-		TetrisStart.myMain();
-		System.out.println("Test2");
+	private void setApplicationName() throws UnsupportedEncodingException {
+		String path = WissPlayStore.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		String decodedPath = URLDecoder.decode(path, "UTF-8");
+		System.out.println(decodedPath);
+		String[] pathArray = decodedPath.split("/");
+		applicationName = pathArray[pathArray.length-1];
+		System.out.println(applicationName);
 	}
-	public void startMoorhuhn() {
-		Moorhuhn.myMain();
-	}
-	public void startSchiffliversenken() {
-		Schiffliversenken.myMain();
-	}
+
+
+	
 }
