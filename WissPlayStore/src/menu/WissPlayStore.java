d7b49c17f8cf6f157f19399cff8041a6434127d6
Lenny5156 <35994864+Lenny5156@users.noreply.github.com>
2018-07-04 11:39:11 +0200

    Reformated

--- WissPlayStore/src/menu/WissPlayStore.java
+++ WissPlayStore/src/menu/WissPlayStore.java
@@ -1,6 +1,5 @@
 package menu;
 
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -12,18 +11,17 @@ import processing.core.PApplet;
 
 /**
  * 
- * @author MaPf06
- * [Ljava.lang.String;@1540e19d
+ * @author MaPf06 [Ljava.lang.String;@1540e19d
  * 
  */
 
 public class WissPlayStore extends PApplet {
-	
-	String[] games = {"Tetris","Moorhuhn","Schiffliversänkä","TextAdventure N/A"};
+
+	String[] games = { "Tetris", "Moorhuhn", "Schiffliversänkä", "TextAdventure N/A" };
 	Button[][] button;
 	String applicationName;
 	int arraySize;
-	
+
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			PApplet.main("menu.WissPlayStore");
@@ -44,11 +42,13 @@ public class WissPlayStore extends PApplet {
 			}
 		}
 	}
-	//Sets up the window
+
+	// Sets up the window
 	public void settings() {
-		size(800,800);
+		size(800, 800);
 	}
-	//Sets up the buttons
+
+	// Sets up the buttons
 	public void setup() {
 		try {
 			setApplicationName();
@@ -58,35 +58,37 @@ public class WissPlayStore extends PApplet {
 		}
 		width = 800;
 		arraySize = (int) Math.ceil(Math.sqrt(games.length));
-		int size = (width-(10*arraySize)+10)/arraySize;
+		int size = (width - (10 * arraySize) + 10) / arraySize;
 		button = new Button[arraySize][arraySize];
 		for (int i = 0; i < arraySize; i++) {
 			for (int j = 0; j < arraySize; j++) {
-				if (i*arraySize + j < games.length) {
-					button[i][j] = new Button(this, (size+10)*j,(size+10)*i,size,size);
+				if (i * arraySize + j < games.length) {
+					button[i][j] = new Button(this, (size + 10) * j, (size + 10) * i, size, size);
 					button[i][j].changeColor(150);
-					button[i][j].text = games[i*arraySize + j];
-					button[i][j].textSize = size/10;
+					button[i][j].text = games[i * arraySize + j];
+					button[i][j].textSize = size / 10;
 				}
 			}
 		}
 	}
-	
-	//Draws the GUI
+
+	// Draws the GUI
 	public void draw() {
 		background(75);
 		for (int i = 0; i < arraySize; i++) {
 			for (int j = 0; j < arraySize; j++) {
-				if (i*arraySize + j < games.length) {
-					if (button[i][j].checkOnButton(mouseX, mouseY)) button[i][j].changeColor(255);
-					else button[i][j].changeColor(150);
+				if (i * arraySize + j < games.length) {
+					if (button[i][j].checkOnButton(mouseX, mouseY))
+						button[i][j].changeColor(255);
+					else
+						button[i][j].changeColor(150);
 					button[i][j].drawButton();
 				}
 			}
 		}
 	}
 
-	//When the mouse gets clicked it will start the game
+	// When the mouse gets clicked it will start the game
 	public void mouseClicked() {
 		try {
 			startGame();
@@ -94,38 +96,36 @@ public class WissPlayStore extends PApplet {
 			e.printStackTrace();
 		}
 	}
-	
-	//Starts the game
-	//This application will be startet with the game name as args
+
+	// Starts the game
+	// This application will be startet with the game name as args
 	public void startGame() throws IOException {
 		String gameName = null;
-		
+
 		for (int i = 0; i < arraySize; i++) {
 			for (int j = 0; j < arraySize; j++) {
-				if (i*arraySize + j < games.length) {
-					if (button[i][j].checkOnButton(mouseX,mouseY)) {
+				if (i * arraySize + j < games.length) {
+					if (button[i][j].checkOnButton(mouseX, mouseY)) {
 						gameName = button[i][j].text;
 						i = button.length;
 					}
 				}
 			}
 		}
-		
+
 		if (gameName != null) {
 			Runtime rt = Runtime.getRuntime();
-			rt.exec("cmd.exe /c start java -jar "+applicationName+" "+gameName);
+			rt.exec("cmd.exe /c start java -jar " + applicationName + " " + gameName);
 		}
 	}
-	
+
 	private void setApplicationName() throws UnsupportedEncodingException {
 		String path = WissPlayStore.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		String decodedPath = URLDecoder.decode(path, "UTF-8");
 		System.out.println(decodedPath);
 		String[] pathArray = decodedPath.split("/");
-		applicationName = pathArray[pathArray.length-1];
+		applicationName = pathArray[pathArray.length - 1];
 		System.out.println(applicationName);
 	}
 
-
-	
 }
